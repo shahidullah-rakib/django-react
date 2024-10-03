58abc26c056a9c228d9518dfb48c9837ea2ce41f
DESKTOP-8TBDQ8N\USER <rakib.ygbl@gmail.com>
2024-10-03 13:31:48 +0600

    add the loading

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,7 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import api from '../api';
+import Note from '../components/Note';
+import '../styles/Home.css';
 
 function Home() {
   const [notes, setNotes] = useState([]);
@@ -23,15 +25,13 @@ function Home() {
 
   const deleteNote = (id) => {
     api
-      .delete(`/api/notes/${id}/`)
+      .delete(`/api/notes/delete/${id}/`)
       .then((res) => {
-        if (res.status === 204) {
-          alert('Note Deleted Successful');
-        } else {
-          alert('Failed to delete note');
-        }
+        if (res.status === 204) alert('Note deleted!');
+        else alert('Failed to delete note.');
+        getNotes();
       })
-      .catch((err) => alert(err));
+      .catch((error) => alert(error));
   };
 
   const createNote = (e) => {
@@ -50,9 +50,9 @@ function Home() {
     <div>
       <div>
         <h2>Notes</h2>
-        {notes.map((note) => {
-          <p>{note}</p>;
-        })}
+        {notes.map((note) => (
+          <Note note={note} onDelete={deleteNote} key={note.id} />
+        ))}
       </div>
       <h2>Create a Note</h2>
       <form onSubmit={createNote}>
@@ -62,18 +62,18 @@ function Home() {
           type="text"
           id="title"
           name="title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
           required
+          onChange={(e) => setTitle(e.target.value)}
+          value={title}
         />
         <label htmlFor="content">Content:</label>
         <br />
         <textarea
           id="content"
           name="content"
+          required
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          required
         ></textarea>
         <br />
         <input type="submit" value="Submit"></input>
