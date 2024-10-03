22d53e331194475ccc0859bbb7b7685e3be15cfa
DESKTOP-8TBDQ8N\USER <rakib.ygbl@gmail.com>
2024-10-03 12:38:32 +0600

    buoild function for fetch,create and delete notes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,53 @@
+import { useEffect, useState } from 'react';
+import api from '../api';
+
 function Home() {
+  const [notes, setNotes] = useState([]);
+  const [content, setContent] = useState('');
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    getNotes();
+  }, []);
+
+  const getNotes = () => {
+    api
+      .get('/api/notes/')
+      .then((res) => res.data)
+      .then((data) => {
+        setNotes(data);
+        console.log(data);
+      })
+      .catch((err) => alert(err));
+  };
+
+  const deleteNote = (id) => {
+    api
+      .delete(`/api/notes/${id}/`)
+      .then((res) => {
+        if (res.status === 204) {
+          alert('Note Deleted Successful');
+        } else {
+          alert('Failed to delete note');
+        }
+      })
+      .catch((err) => alert(err));
+  };
+
+  const createNote = (e) => {
+    e.preventdefault();
+    api
+      .post('api/notes/', { content, title })
+      .then((res) => {
+        if (res.status === 201) {
+          alert('Note Created');
+        } else {
+          alert('Failed to create Note');
+        }
+      })
+      .catch((err) => alert(err));
+  };
+
   return (
     <div>
       <div>
